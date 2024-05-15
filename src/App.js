c8ec963dacb5433c0f737e6f8a8f2bc369f82fc2
VISHALVKY <vishalvky1510@gmail.com>
2024-05-15 12:55:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [markdown, setMarkdown] = useState("");
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <textarea onChange={handleChange} />
+
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
-
-export default App;
